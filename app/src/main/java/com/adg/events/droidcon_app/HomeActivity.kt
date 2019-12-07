3a65ba344164530227666be4ce21e77b3039f822
vjgarciag96 <vjgarcia@tuenti.com>
2019-12-08 00:38:03 +0100

    Avoid showing wrong fragment when a configuration change happens

--- app/src/main/java/com/adg/events/droidcon_app/HomeActivity.kt
+++ app/src/main/java/com/adg/events/droidcon_app/HomeActivity.kt
@@ -14,6 +14,14 @@ class HomeActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        if(savedInstanceState == null) {
+            setUpNavigationBar()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
         setUpNavigationBar()
     }
 
