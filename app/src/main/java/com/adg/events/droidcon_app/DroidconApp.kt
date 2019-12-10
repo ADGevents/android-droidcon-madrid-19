fdf9049770805685d631aabe941690cca6c664c3
vjgarciag96 <vjgarcia@tuenti.com>
2019-12-10 18:31:35 +0100

    Remove flipper

--- app/src/main/java/com/adg/events/droidcon_app/DroidconApp.kt
+++ app/src/main/java/com/adg/events/droidcon_app/DroidconApp.kt
@@ -1,12 +1,6 @@
 package com.adg.events.droidcon_app
 
 import com.adg.events.droidcon_app.ioc.DaggerDroidconAppComponent
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -15,15 +9,7 @@ class DroidconApp : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
         AndroidThreeTen.init(this)
-
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-            val client = AndroidFlipperClient.getInstance(this)
-            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-            client.addPlugin(DatabasesFlipperPlugin(this))
-            client.start()
-        }
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
