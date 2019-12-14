43767b1aab514614122857de270e0f25d9b08cbc
vjgarciag96 <vjgarciaw@gmail.com>
2019-12-14 19:43:44 +0100

    Info tab. Add links to code of conduct and website, contacts of interests and venue map.

--- info/src/main/java/com/droidcon/info/presentation/InfoFragment.kt
+++ info/src/main/java/com/droidcon/info/presentation/InfoFragment.kt
@@ -1,30 +1,29 @@
 package com.droidcon.info.presentation
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.ViewPager
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.droidcon.commons.recyclerview.setDivider
 import com.droidcon.info.R
 import com.droidcon.info.presentation.viewmodel.InfoFragmentViewModel
 import com.droidcon.info.presentation.viewmodel.InfoFragmentViewModelFactory
-import com.google.android.material.tabs.TabLayout
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
 class InfoFragment : DaggerFragment() {
 
-    private lateinit var infoOptionsAdapter: InfoOptionsAdapter
-
     @Inject
     lateinit var infoFragmentViewModelFactory: InfoFragmentViewModelFactory
     private lateinit var infoFragmentViewModel: InfoFragmentViewModel
 
-    private lateinit var infoTabs: TabLayout
-    private lateinit var infoOptionsViewPager: ViewPager
+    @Inject
+    lateinit var infoOptionsAdapter: InfoOptionsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,23 +36,49 @@ class InfoFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        infoTabs = view.findViewById(R.id.infoTabs)
-        infoOptionsViewPager = view.findViewById(R.id.infoOptionsViewPage)
-        infoTabs.setupWithViewPager(infoOptionsViewPager)
-        infoOptionsViewPager.offscreenPageLimit = INFO_TABS.count() - 1
-        infoOptionsAdapter = InfoOptionsAdapter(childFragmentManager)
-        infoOptionsViewPager.adapter = infoOptionsAdapter
+
+        view.findViewById<RecyclerView>(R.id.infoOptions).run {
+            layoutManager = LinearLayoutManager(context)
+            adapter = infoOptionsAdapter
+            setDivider(R.drawable.row_divider)
+        }
+
+        bindViewModel()
     }
 
-
-    inner class InfoOptionsAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-
-        override fun getItem(position: Int): Fragment = InfoOptionFragment()
-
-        override fun getCount(): Int = INFO_TABS.count()
+    private fun bindViewModel() {
+        infoFragmentViewModel.infoState.observe(::getLifecycle) { state ->
+            infoOptionsAdapter.submitList(state.infoOptions)
+        }
+        infoFragmentViewModel.infoEffects.observe(::getLifecycle, ::onInfoEffect)
+        infoFragmentViewModel.onInfoVisible()
     }
 
-    private companion object {
-        val INFO_TABS = listOf("Map", "Code of conduct")
+    private fun onInfoEffect(infoEffect: InfoEffect) {
+        when (infoEffect) {
+            is InfoEffect.NavigateToUri -> navigateToUri(infoEffect.url)
+            is InfoEffect.NavigateToMaps -> navigateToMaps(infoEffect.mapsInfo)
+            InfoEffect.NavigateToContactsOfInterest -> navigateToContactsOfInterest()
+        }
+    }
+
+    private fun navigateToUri(uri: Uri) {
+        Intent(Intent.ACTION_VIEW).run {
+            data = uri
+            startActivity(this)
+        }
+    }
+
+    private fun navigateToMaps(mapsInfo: Uri) {
+        Intent(Intent.ACTION_VIEW, mapsInfo).run {
+            setPackage("com.google.android.apps.maps")
+            startActivity(this)
+        }
+    }
+
+    private fun navigateToContactsOfInterest() {
+        findNavController().navigate(
+            InfoFragmentDirections.actionInfoFragmentToContactsOfInterestFragment()
+        )
     }
 }
--- info/src/main/java/com/droidcon/info/presentation/InfoOptionFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.droidcon.info.presentation
-
-import androidx.fragment.app.Fragment
-import dagger.android.support.DaggerFragment
-
-class InfoOptionFragment : Fragment() {
-}
