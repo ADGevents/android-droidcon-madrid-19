19463b66658713f5f33929376d877e2b3c03724c
Jorge Mucientes <jmucientes@tuenti.com>
2019-12-14 02:56:49 +0100

    Add speakers to session detail

--- schedule/src/main/java/com/droidcon/schedule/ui/sessiondetail/fragment/SessionDetailFragment.kt
+++ schedule/src/main/java/com/droidcon/schedule/ui/sessiondetail/fragment/SessionDetailFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.droidcon.schedule.R
+import com.droidcon.schedule.ui.sessiondetail.model.SessionSpeakerRow
 import com.droidcon.schedule.ui.sessiondetail.viewmodel.SessionDetailViewModel
 import com.droidcon.schedule.ui.sessiondetail.viewmodel.SessionDetailViewModelFactory
 import dagger.android.support.DaggerFragment
@@ -17,6 +20,7 @@ class SessionDetailFragment : DaggerFragment() {
     lateinit var sessionDetailViewModelFactory: SessionDetailViewModelFactory
     private lateinit var sessionDetailViewModel: SessionDetailViewModel
 
+    private lateinit var sessionDetailsContainer: ViewGroup
     private lateinit var sessionTitle: TextView
     private lateinit var sessionDescription: TextView
     private lateinit var sessionDuration: TextView
@@ -41,6 +45,7 @@ class SessionDetailFragment : DaggerFragment() {
     }
 
     private fun bindViews(view: View) {
+        sessionDetailsContainer = view.findViewById(R.id.sessionDetailsContainer)
         sessionTitle = view.findViewById(R.id.sessionTitle)
         sessionDuration = view.findViewById(R.id.sessionDetailDuration)
         sessionRoom = view.findViewById(R.id.sessionDetailRoom)
@@ -51,8 +56,27 @@ class SessionDetailFragment : DaggerFragment() {
         sessionDetailViewModel.sessionDetailState.observe(::getLifecycle) { sessionDetail ->
             sessionTitle.text = sessionDetail.title
             sessionDuration.text = sessionDetail.duration
-            sessionRoom.text = sessionDetail.roomName
+            sessionRoom.text = "Room: ${sessionDetail.roomName}"
             sessionDescription.text = sessionDetail.description
+            sessionDetail.speakers.forEach { addSessionSpeakerRow(it) }
         }
+    }
+
+    private fun addSessionSpeakerRow(sessionSpeaker: SessionSpeakerRow) {
+        val speakerRow = LayoutInflater.from(context)
+            .inflate(R.layout.session_row, sessionDetailsContainer, false)
+
+        speakerRow.findViewById<TextView>(R.id.speakerName).text = sessionSpeaker.fullName
+        speakerRow.findViewById<TextView>(R.id.speakerDescription).text = sessionSpeaker.tagLine
+
+        context?.let {
+            Glide.with(it)
+                .load(sessionSpeaker.profilePicture)
+                .placeholder(R.drawable.ic_default_avatar)
+                .transform(CircleCrop())
+                .into(speakerRow.findViewById(R.id.speakerAvatar))
+        }
+
+        sessionDetailsContainer.addView(speakerRow)
     }
 }
